600980695889305dbf737be7b2c7dc253bd451fb
michbur <michalburdukiewicz@gmail.com>
2015-10-10 20:49:05 +0200

    added more encodings based on 1 to 5 traits

--- create_encodings.R
+++ create_encodings.R
@@ -5,18 +5,27 @@ source("choose_properties.R")
 
 grouping_properties <- aa_nprop[unlist(traits), ]
 
-all_traits_combn_list <- list(expand.grid(traits))
+#vector of traits
+vtraits <- unlist(traits)
 
-aa_groups <- unlist(lapply(all_traits_combn_list, function(all_traits_combn)
-  lapply(3L:6, function(single_k)
+#all combinations of traits
+all_traits_combn_list <- lapply(1L:5, function(i)
+  t(combn(vtraits, i)))
+
+
+aa_groups <- lapply(3L:6, function(single_k)
+  lapply(all_traits_combn_list, function(all_traits_combn)
     lapply(1L:nrow(all_traits_combn), function(single_trait_combn) {
-      cl <- hclust(dist(t(aa_nprop[unlist(all_traits_combn[single_trait_combn, ]), ])))
+      cl <- t(aa_nprop[unlist(all_traits_combn[single_trait_combn, , drop = FALSE]), , drop = FALSE]) %>%
+        dist %>%
+        hclust(method = "ward.D2")
+      #cl <- hclust(dist(t(aa_nprop[unlist(all_traits_combn[single_trait_combn, , drop = FALSE]), , drop = FALSE])))
       gr <- cutree(cl, k = single_k)
       names(gr) <- tolower(names(gr))
       agg_gr <- lapply(unique(gr), function(single_group) names(gr[gr == single_group]))
       names(agg_gr) <- 1L:length(agg_gr)
       agg_gr
-    }))), recursive = FALSE)
+    })) %>% unlist(recursive = FALSE))
 
 aa_id <- lapply(aa_groups, function(j) {
   sort_gr <- lapply(j, function(i) {
@@ -30,15 +39,6 @@ aa_groups <- unlist(lapply(1L:length(aa_id), function(i) {
   aa_groups[[i]][aa_id[[i]]]
 }), recursive = FALSE)
 
-aa_groups <- sapply(aa_groups, function(i) {
-  res <- sapply(i, sort)
-  res[order(lengths(res))]
-})
-
-groups_summary <- cbind(chosen = !duplicated(t(sapply(aa_groups, unlist))), do.call(rbind, lapply(3L:6, function(i) 
-  cbind(n = rep(i, nrow(all_traits_combn_list[[1]])), all_traits_combn_list[[1]]))))
-
-aa_groups <- aa_groups[!duplicated(t(sapply(aa_groups, unlist)))]
 
 aa1 = list(`1` = c("g", "a", "p", "v", "l", "i", "m"), 
            `2` = c("k", "r", "h"), 
