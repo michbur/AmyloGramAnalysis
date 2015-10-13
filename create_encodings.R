b2697d827bf74a3d9641c3fb77c6d35366e8ec33
michbur <michalburdukiewicz@gmail.com>
2015-10-13 21:03:28 +0200

    analyzed features which duplicate encodings

--- create_encodings.R
+++ create_encodings.R
@@ -12,13 +12,8 @@ vtraits <- unlist(traits)
 all_traits_combn_list <- lapply(1L:15, function(i)
   t(combn(vtraits, i)))
 
-#table of features-encodings (features on which encodings are based)
-fenc_table <- do.call(rbind, lapply(all_traits_combn_list, function(i)
-  cbind(i, matrix(NA, nrow = nrow(i), ncol = length(vtraits) - ncol(i)))))
-rownames(fenc_table) <- paste0("ID", 1L:nrow(enc_table))
-
 #create encodings
-aa_groups <- lapply(3L:6, function(single_k) {
+all_aa_groups <- lapply(3L:6, function(single_k) {
   res <- lapply(all_traits_combn_list, function(all_traits_combn)
     lapply(1L:nrow(all_traits_combn), function(single_trait_combn) {
       cl <- t(aa_nprop[unlist(all_traits_combn[single_trait_combn, , drop = FALSE]), , drop = FALSE]) %>%
@@ -29,25 +24,24 @@ aa_groups <- lapply(3L:6, function(single_k) {
       names(gr) <- tolower(names(gr))
       agg_gr <- lapply(unique(gr), function(single_group) names(gr[gr == single_group]))
       names(agg_gr) <- 1L:length(agg_gr)
-      agg_gr
+      #inside encodings, amino acids are ordered alphabetically
+      agg_gr <- lapply(agg_gr, sort)
+      #groups are sorted by their length
+      agg_gr[order(lengths(agg_gr))]
     })) %>% unlist(recursive = FALSE) 
   names(res) <- paste0("ID", 1L:length(res), "K", single_k)
   res
 })
 
 #get indices of unique encodings
-aa_id <- lapply(aa_groups, function(j) {
-  sort_gr <- lapply(j, function(i) {
-    res <- sapply(i, sort)
-    res[order(lengths(res))]
-  })
-  !duplicated(t(sapply(sort_gr, unlist)))
-})
+aa_id <- lapply(all_aa_groups, function(i) !duplicated(t(sapply(i, unlist))))
 
+#remove from aa_groups redundant encodings
 aa_groups <- unlist(lapply(1L:length(aa_id), function(i) {
-  aa_groups[[i]][aa_id[[i]]]
+  all_aa_groups[[i]][aa_id[[i]]]
 }), recursive = FALSE)
 
+#add as a benchmark two encodings from the literature
 aa1 = list(`1` = c("g", "a", "p", "v", "l", "i", "m"), 
            `2` = c("k", "r", "h"), 
            `3` = c("d", "e"), 
