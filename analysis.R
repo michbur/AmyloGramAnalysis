dfc09bbab403bfa22d6961350cb2cfb0b3d77e46
michbur <michalburdukiewicz@gmail.com>
2015-11-08 21:36:43 +0100

    fixed extraction of n-grams again

--- analysis.R
+++ analysis.R
@@ -8,5 +8,6 @@ ftraits <- choose_properties()
 #aa_groups <- create_encodings(ftraits)
 #save(aa_groups, file = "aa_groups.RData")
 load("aa_groups.RData")
-
 aa_groups <- vector2list(aa_groups)
+#need to add seq
+extract_ngrams(seq, aa_groups)
--- functions/encode_amyloids.R
+++ functions/encode_amyloids.R
@@ -7,18 +7,16 @@ vector2list <- function(x){
   }) %>% lapply(first)
 }
 
-create_gl <- function(seq)
-  lapply(1L:nrow(seq), function(i) {
+
+
+extract_ngrams <- function(seq, aa_groups) {
+  require(biogram)
+  
+  gl <- lapply(1L:nrow(seq), function(i) {
     res <- do.call(rbind, strsplit(decode_ngrams(seq2ngrams(seq[i, ][!is.na(seq[i, ])], 6, a()[-1])), ""))
     cbind(res, id = paste0("P", rep(i, nrow(res))))
   })
-
-
-pos_gl <- create_gl(pos_data)
-neg_gl <- create_gl(neg_data)
-
-extract_ngrams <- function(gl) {
-  require(biogram)
+  
   lapply(aa_groups, function(single_group) 
     lapply(gl, function(single_protein) {
       bitrigrams <- as.matrix(count_multigrams(ns = c(1, rep(2, 4), rep(3, 3)), 
@@ -34,16 +32,8 @@ extract_ngrams <- function(gl) {
   )
 }
 
-coded_neg <- lapply(aa_groups, function(single_group) {
-  lapply(neg_gl, function(single_protein) {
-    bitrigrams <- as.matrix(count_multigrams(ns = c(1, rep(2, 4), rep(3, 3)), 
-                                             ds = list(0, 0, 1, 2, 3, c(0, 0), c(0, 1), c(1, 0)),
-                                             seq = degenerate(single_protein[, -7], single_group),
-                                             u = as.character(1L:length(single_group))))
-    
-    bitrigrams <- bitrigrams > 0
-    storage.mode(bitrigrams) <- "integer"
-    
-    bitrigrams
-  })
-})
+
+
+pos_gl <- extract_ngrams(pos_data)
+neg_gl <- extract_ngrams(neg_data)
+
