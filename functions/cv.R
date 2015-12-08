284ca11ba358622e1952b1c7c9f712d1ffc0af0d
michbur <michalburdukiewicz@gmail.com>
2015-12-08 22:32:34 +0100

    forgot to commit changes in cv

--- functions/cv.R
+++ functions/cv.R
@@ -2,35 +2,28 @@
 
 fold_res <- lapply(c(6, 10, 15), function(constant) {
   
-  pos_train <- which(ets == 1 & seq_lengths < 7)
-  neg_train <- which(ets == 0 & seq_lengths < 16)
-  
-  fold_list_train <- lapply(list(pos_train, neg_train), function(single_n) {
-    folded <- cvFolds(length(single_n), K = 5)
-    data.frame(id = single_n[folded[["subsets"]]], which = folded[["which"]])
-  })
-  
-  pos_test <- which(ets == 1 & seq_lengths >= 7)
-  neg_test <- which(ets == 0 & seq_lengths >= 16)
-
-  #test only splitted in four
-  fold_list_test <- lapply(list(pos_test, neg_test), function(single_n) {
-    folded <- cvFolds(length(single_n), K = 4)
-    data.frame(id = single_n[folded[["subsets"]]], which = folded[["which"]])
-  })
-  
   lapply(1L:8, function(dummy) {
 
+    pos_train <- which(ets == 1 & seq_lengths < 7)
+    neg_train <- which(ets == 0 & seq_lengths < 16)
+    
+    pos_test <- which(ets == 1 & seq_lengths >= 7)
+    neg_test <- which(ets == 0 & seq_lengths >= 16)
+    
+    fold_list <- lapply(list(pos_train, pos_test, neg_train, neg_test), function(single_n) {
+      folded <- cvFolds(length(single_n), K = 5)
+      data.frame(id = single_n[folded[["subsets"]]], which = folded[["which"]])
+    })
     
     lapply(1L:5, function(fold) {
       lapply(1L:length(aa_groups), function(group_id) {
-        train_pos <- do.call(rbind, coded_pos[[group_id]][fold_list[[1]][fold_list[[1]][, "which"] != fold, "id"]])
-        train_neg <- do.call(rbind, coded_neg[[group_id]][fold_list[[3]][fold_list[[3]][, "which"] != fold, "id"]])
+        train_pos <- do.call(rbind, coded[[group_id]][fold_list[[1]][fold_list[[1]][, "which"] != fold, "id"]])
+        train_neg <- do.call(rbind, coded[[group_id]][fold_list[[3]][fold_list[[3]][, "which"] != fold, "id"]])
         
-        test_pos <- rbind(do.call(rbind, coded_pos[[group_id]][fold_list[[1]][fold_list[[1]][, "which"] == fold, "id"]]),
-                          do.call(rbind, coded_pos[[group_id]][fold_list[[2]][fold_list[[2]][, "which"] == fold, "id"]]))
-        test_neg <- rbind(do.call(rbind, coded_neg[[group_id]][fold_list[[3]][fold_list[[3]][, "which"] == fold, "id"]]), 
-                          do.call(rbind, coded_neg[[group_id]][fold_list[[4]][fold_list[[4]][, "which"] == fold, "id"]]))
+        test_pos <- rbind(do.call(rbind, coded[[group_id]][fold_list[[1]][fold_list[[1]][, "which"] == fold, "id"]]),
+                          do.call(rbind, coded[[group_id]][fold_list[[2]][fold_list[[2]][, "which"] == fold, "id"]]))
+        test_neg <- rbind(do.call(rbind, coded[[group_id]][fold_list[[3]][fold_list[[3]][, "which"] == fold, "id"]]), 
+                          do.call(rbind, coded[[group_id]][fold_list[[4]][fold_list[[4]][, "which"] == fold, "id"]]))
         
         test_bis <- test_features(c(rep(1, nrow(train_pos)), rep(0, nrow(train_neg))),
                                   rbind(train_pos, train_neg), adjust = NULL)
@@ -41,10 +34,10 @@ fold_res <- lapply(c(6, 10, 15), function(constant) {
                                  as.factor(c(rep(1, nrow(train_pos)), rep(0, nrow(train_neg)))))
         
         #number of n-grams from protein
-        ngram_prots_pos <- c(sapply(coded_pos[[group_id]][fold_list[[1]][fold_list[[1]][, "which"] == fold, "id"]], nrow),
-                             sapply(coded_pos[[group_id]][fold_list[[2]][fold_list[[2]][, "which"] == fold, "id"]], nrow))
-        ngram_prots_neg <- c(sapply(coded_neg[[group_id]][fold_list[[3]][fold_list[[3]][, "which"] == fold, "id"]], nrow),
-                             sapply(coded_neg[[group_id]][fold_list[[4]][fold_list[[4]][, "which"] == fold, "id"]], nrow))
+        ngram_prots_pos <- c(sapply(coded[[group_id]][fold_list[[1]][fold_list[[1]][, "which"] == fold, "id"]], nrow),
+                             sapply(coded[[group_id]][fold_list[[2]][fold_list[[2]][, "which"] == fold, "id"]], nrow))
+        ngram_prots_neg <- c(sapply(coded[[group_id]][fold_list[[3]][fold_list[[3]][, "which"] == fold, "id"]], nrow),
+                             sapply(coded[[group_id]][fold_list[[4]][fold_list[[4]][, "which"] == fold, "id"]], nrow))
         
         preds <- cbind(predict(model_cv, rbind(test_pos, test_neg), type = "prob")[, 2], 
                        c(rep(1, nrow(test_pos)), rep(0, nrow(test_neg))),
