698629254bae0704ce805516964b85ac3d992c5c
michbur <michalburdukiewicz@gmail.com>
2015-12-14 09:31:04 +0100

    investing normalization for distance measures in calc_ed

--- calc_ed_musings.R
+++ calc_ed_musings.R
@@ -1,10 +1,12 @@
-b <- aa_groups[[5]]
-a <- aa_groups[[5000]]
+bi <- 50
+ai <- 5000
+b <- aa_groups[[bi]]
+a <- aa_groups[[ai]]
 
 library(biogram)
 calc_ed(a, b)
 
-traits <- create_traits_combination(vtraits)
+traits <- create_traits_combination(ftraits)
 
 traits_table <- do.call(rbind, lapply(traits, function(i) 
   cbind(i, matrix(NA, ncol = 17 - ncol(i), nrow = nrow(i)))))
@@ -20,5 +22,23 @@ traits_table[5000, ]
 
 aa_nprop <- normalize_properties()
 
-a_prop <- aa_nprop[na.omit(traits_table[5000, ]), , drop = FALSE]
-b_prop <- aa_nprop[na.omit(traits_table[5, ]), , drop = FALSE]
+a_prop <- aa_nprop[na.omit(traits_table[ai, ]), , drop = FALSE]
+colnames(a_prop) <- tolower(colnames(a_prop))
+
+#b_prop <- aa_nprop[na.omit(traits_table[ao, ]), , drop = FALSE]
+
+# must have unified lists of features
+
+coords_a <- lapply(a, function(single_subgroup) rowMeans(a_prop[, single_subgroup, drop = FALSE]))
+coords_b <- lapply(b, function(single_subgroup) rowMeans(a_prop[, single_subgroup, drop = FALSE]))
+
+# finished - distance as the minimum difference in properties between groups
+# how to deal with encodings with the unequal number of groups
+sum(sapply(coords_a, function(single_coords_a) {
+  distances <- sapply(coords_b, function(single_coords_b) 
+    #vector of distances between groups
+    sqrt(sum((single_coords_a - single_coords_b)^2))
+  )
+  #c(dist = min(distances), id = unname(which.min(distances)))
+  min(distances)
+}))
