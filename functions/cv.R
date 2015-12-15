83ac07f95275efa5f0b442d22e2c5f9eb0810a64
michbur <michalburdukiewicz@gmail.com>
2015-12-15 12:00:55 +0100

    improved cross-validation

--- functions/cv.R
+++ functions/cv.R
@@ -4,7 +4,8 @@
 #' amylyoids and non-amyloids.
 #' @param extracted_ngrams a list of n-grams as fiven by \code{\link{extract_ngrams}}.
 #' @param ets a vector of etiquettes (0 or 1).
-#' @param ets a vector of lengths of sequences.
+#' @param ets a vector of lengths of sequences (must be provided, since we do not use
+#' exact sequences and calculating length from n-gram is inconvenient)).
 #' 
 #' @note The classifier of choice is the \code{\link{[ranger]ranger}} implementation of 
 #' random forests algorithm.
@@ -17,59 +18,65 @@
 
 do_cv <- function(extracted_ngrams, ets, seq_lengths) {
   
-  pos_train <- which(ets == 1 & seq_lengths < 7)
-  neg_train <- which(ets == 0 & seq_lengths < 16)
   
-  pos_test <- which(ets == 1 & seq_lengths >= 7)
-  neg_test <- which(ets == 0 & seq_lengths >= 16)
-  
-  lapply(1L:2, function(dummy) {
-    
-    fold_list <- lapply(list(pos_train, pos_test, neg_train, neg_test), function(single_n) {
-      folded <- cvFolds(length(single_n), K = 5)
-      data.frame(id = single_n[folded[["subsets"]]], which = folded[["which"]])
-    })
-    
-    lapply(1L:5, function(fold) {
-      lapply(extracted_ngrams, function(encoded_group) {
+  lapply(c(6, 10, 15), function(constant_pos) {
+    lapply(c(6, 10, 15), function(constant_neg) {
+      
+      pos_train <- which(ets == 1 & seq_lengths < constant_pos)
+      neg_train <- which(ets == 0 & seq_lengths < constant_neg)
+      
+      pos_test <- which(ets == 1 & seq_lengths >= constant_pos)
+      neg_test <- which(ets == 0 & seq_lengths >= constant_neg)
+      
+      lapply(1L:10, function(dummy) {
         
-        #training data
-        train_pos <- do.call(rbind, encoded_group[fold_list[[1]][fold_list[[1]][, "which"] != fold, "id"]])
-        train_neg <- do.call(rbind, encoded_group[fold_list[[3]][fold_list[[3]][, "which"] != fold, "id"]])
+        fold_list <- lapply(list(pos_train, pos_test, neg_train, neg_test), function(single_n) {
+          folded <- cvFolds(length(single_n), K = 5)
+          data.frame(id = single_n[folded[["subsets"]]], which = folded[["which"]])
+        })
         
-        test_bis <- test_features(c(rep(1, nrow(train_pos)), rep(0, nrow(train_neg))),
-                                  rbind(train_pos, train_neg), adjust = NULL)
-        imp_bigrams <- cut(test_bis, breaks = c(0, 0.05, 1))[[1]]
-        
-        train_data <- data.frame(rbind(train_pos, train_neg)[, imp_bigrams],
-                                 tar = as.factor(c(rep(1, nrow(train_pos)), rep(0, nrow(train_neg)))))
-        model_cv <- ranger(tar ~ ., train_data, write.forest = TRUE)
-        
-        #test data
-        test_pos <- rbind(do.call(rbind, encoded_group[fold_list[[1]][fold_list[[1]][, "which"] == fold, "id"]]),
-                          do.call(rbind, encoded_group[fold_list[[2]][fold_list[[2]][, "which"] == fold, "id"]]))
-        test_neg <- rbind(do.call(rbind, encoded_group[fold_list[[3]][fold_list[[3]][, "which"] == fold, "id"]]), 
-                          do.call(rbind, encoded_group[fold_list[[4]][fold_list[[4]][, "which"] == fold, "id"]]))
-        
-        #number of n-grams from protein
-        ngram_prots_pos <- c(sapply(encoded_group[fold_list[[1]][fold_list[[1]][, "which"] == fold, "id"]], nrow),
-                             sapply(encoded_group[fold_list[[2]][fold_list[[2]][, "which"] == fold, "id"]], nrow))
-        ngram_prots_neg <- c(sapply(encoded_group[fold_list[[3]][fold_list[[3]][, "which"] == fold, "id"]], nrow),
-                             sapply(encoded_group[fold_list[[4]][fold_list[[4]][, "which"] == fold, "id"]], nrow))
-        
-        preds <- cbind(as.numeric(as.character(predict(model_cv, data.frame(rbind(test_pos, test_neg)[, imp_bigrams]))[["predictions"]])), 
-                       c(rep(1, nrow(test_pos)), rep(0, nrow(test_neg))),
-                       c(unlist(lapply(1L:length(ngram_prots_pos), function(prot_id)
-                         rep(prot_id, ngram_prots_pos[prot_id]))), 
-                         unlist(lapply(1L:length(ngram_prots_neg), function(prot_id)
-                           rep(prot_id, ngram_prots_neg[prot_id])))))
-        
-        # is randomForest consistent with ranger?
-        # model_rf <- randomForest(tar ~ ., train_data)
-        # predict(model_rf, data.frame(rbind(test_pos, test_neg)[, imp_bigrams])) == 
-        #   predict(model_cv, data.frame(rbind(test_pos, test_neg)[, imp_bigrams]))[["predictions"]]
-        
-        list(preds, imp_bigrams)
+        lapply(1L:5, function(fold) {
+          lapply(extracted_ngrams, function(encoded_group) {
+            
+            #training data
+            train_pos <- do.call(rbind, encoded_group[fold_list[[1]][fold_list[[1]][, "which"] != fold, "id"]])
+            train_neg <- do.call(rbind, encoded_group[fold_list[[3]][fold_list[[3]][, "which"] != fold, "id"]])
+            
+            test_bis <- test_features(c(rep(1, nrow(train_pos)), rep(0, nrow(train_neg))),
+                                      rbind(train_pos, train_neg), adjust = NULL)
+            imp_bigrams <- cut(test_bis, breaks = c(0, 0.05, 1))[[1]]
+            
+            train_data <- data.frame(rbind(train_pos, train_neg)[, imp_bigrams],
+                                     tar = as.factor(c(rep(1, nrow(train_pos)), rep(0, nrow(train_neg)))))
+            model_cv <- ranger(tar ~ ., train_data, write.forest = TRUE)
+            
+            #test data
+            test_pos <- rbind(do.call(rbind, encoded_group[fold_list[[1]][fold_list[[1]][, "which"] == fold, "id"]]),
+                              do.call(rbind, encoded_group[fold_list[[2]][fold_list[[2]][, "which"] == fold, "id"]]))
+            test_neg <- rbind(do.call(rbind, encoded_group[fold_list[[3]][fold_list[[3]][, "which"] == fold, "id"]]), 
+                              do.call(rbind, encoded_group[fold_list[[4]][fold_list[[4]][, "which"] == fold, "id"]]))
+            
+            #number of n-grams from protein
+            ngram_prots_pos <- c(sapply(encoded_group[fold_list[[1]][fold_list[[1]][, "which"] == fold, "id"]], nrow),
+                                 sapply(encoded_group[fold_list[[2]][fold_list[[2]][, "which"] == fold, "id"]], nrow))
+            ngram_prots_neg <- c(sapply(encoded_group[fold_list[[3]][fold_list[[3]][, "which"] == fold, "id"]], nrow),
+                                 sapply(encoded_group[fold_list[[4]][fold_list[[4]][, "which"] == fold, "id"]], nrow))
+            
+            preds <- cbind(as.numeric(as.character(predict(model_cv, data.frame(rbind(test_pos, test_neg)[, imp_bigrams]))[["predictions"]])), 
+                           c(rep(1, nrow(test_pos)), rep(0, nrow(test_neg))),
+                           c(unlist(lapply(1L:length(ngram_prots_pos), function(prot_id)
+                             rep(prot_id, ngram_prots_pos[prot_id]))), 
+                             unlist(lapply(1L:length(ngram_prots_neg), function(prot_id)
+                               rep(prot_id, ngram_prots_neg[prot_id])))))
+            
+            # is randomForest consistent with ranger?
+            # model_rf <- randomForest(tar ~ ., train_data)
+            # predict(model_rf, data.frame(rbind(test_pos, test_neg)[, imp_bigrams])) == 
+            #   predict(model_cv, data.frame(rbind(test_pos, test_neg)[, imp_bigrams]))[["predictions"]]
+            
+            list(preds, imp_bigrams)
+          })
+        })
       })
     })
   })
