bed9d3807493b68c0e98428644f21c21a1e67430
michbur <michalburdukiewicz@gmail.com>
2015-12-22 10:20:40 +0100

    reworked cv to make it WCSS-friendly

--- analysis.R
+++ analysis.R
@@ -23,15 +23,21 @@ raw_seqs_list <- c(read.fasta("./data/amyloid_pos_full.fasta",seqtype = "AA"),
                read.fasta("./data/amyloid_neg_full.fasta",seqtype = "AA"))
 seqs_list <- raw_seqs_list[lengths(raw_seqs_list) > 5]
 
-ets <- c(rep(1, length(read.fasta("./data/amyloid_pos_full.fasta",seqtype = "AA"))),
-         rep(0, length(read.fasta("./data/amyloid_neg_full.fasta",seqtype = "AA"))))
-ets <- ets[lengths(raw_seqs_list) > 5]
-seq_lengths <- unname(lengths(seqs_list))
+
 
 seqs_m <- tolower(t(sapply(seqs_list, function(i)
   c(i, rep(NA, max(lengths(seqs_list)) - length(i))))))
 
 #for single aa_group around 85 sec
 extracted_ngrams <- extract_ngrams(seqs_m, aa_groups)
+#add to extracted_ngrams raw aa n-grams
 
-cv_results <- do_cv(extracted_ngrams, ets, seq_lengths)
+#create folds for cv
+ets <- c(rep(1, length(read.fasta("./data/amyloid_pos_full.fasta",seqtype = "AA"))),
+         rep(0, length(read.fasta("./data/amyloid_neg_full.fasta",seqtype = "AA"))))
+ets <- ets[lengths(raw_seqs_list) > 5]
+seq_lengths <- unname(lengths(seqs_list))
+
+all_folds <- create_all_folds(ets, seq_lengths)
+
+cv_results <- do_cv(all_folds, extracted_ngrams)
--- functions/cv.R
+++ functions/cv.R
@@ -1,24 +1,18 @@
-#' Perform cross-validation
-#'
-#' Performs cross-validation using the n-grams extracted from 
-#' amylyoids and non-amyloids.
-#' @param extracted_ngrams a list of n-grams as fiven by \code{\link{extract_ngrams}}.
+#' Create folds for cv
+#' 
+#' Returns indices of folds for hexagrams used in cross-validation.
+#' 
 #' @param ets a vector of etiquettes (0 or 1).
-#' @param ets a vector of lengths of sequences (must be provided, since we do not use
+#' @param seq_lengths a vector of lengths of sequences (must be provided, since we do not use
 #' exact sequences and calculating length from n-gram is inconvenient)).
 #' 
-#' @note The classifier of choice is the \code{\link{[ranger]ranger}} implementation of 
-#' random forests algorithm.
-#' 
-#' @return List of length equal to the number of repetitions of cross-validation.
-#' Each element of the list is a list with the number of elements equal to the number 
-#' of encodings. Each element of this list contains a list with two elements:
-#' a) data frame with the prediction, real value of target and id of the protein,
-#' b) list of important n-grams as found by \code{\link[biogram]{test_features}}.
+#' @return List of 90 lists. Each element contains four data frames containing indices for
+#' amyloids in training set, nonamyloids in training set, amyloids in test set and 
+#' nonamyloidsin test set.
 
-do_cv <- function(extracted_ngrams, ets, seq_lengths) {
-  
-  
+create_all_folds <- function(ets, seq_lengths) {
+  #set the seed to alasy create the same folds
+  set.seed(1)
   lapply(c(6, 10, 15), function(constant_pos) {
     lapply(c(6, 10, 15), function(constant_neg) {
       
@@ -35,49 +29,85 @@ do_cv <- function(extracted_ngrams, ets, seq_lengths) {
           data.frame(id = single_n[folded[["subsets"]]], which = folded[["which"]])
         })
         
-        lapply(1L:5, function(fold) {
-          lapply(extracted_ngrams, function(encoded_group) {
-            
-            #training data
-            train_pos <- do.call(rbind, encoded_group[fold_list[[1]][fold_list[[1]][, "which"] != fold, "id"]])
-            train_neg <- do.call(rbind, encoded_group[fold_list[[3]][fold_list[[3]][, "which"] != fold, "id"]])
-            
-            test_bis <- test_features(c(rep(1, nrow(train_pos)), rep(0, nrow(train_neg))),
-                                      rbind(train_pos, train_neg), adjust = NULL)
-            imp_bigrams <- cut(test_bis, breaks = c(0, 0.05, 1))[[1]]
-            
-            train_data <- data.frame(rbind(train_pos, train_neg)[, imp_bigrams],
-                                     tar = as.factor(c(rep(1, nrow(train_pos)), rep(0, nrow(train_neg)))))
-            model_cv <- ranger(tar ~ ., train_data, write.forest = TRUE)
-            
-            #test data
-            test_pos <- rbind(do.call(rbind, encoded_group[fold_list[[1]][fold_list[[1]][, "which"] == fold, "id"]]),
-                              do.call(rbind, encoded_group[fold_list[[2]][fold_list[[2]][, "which"] == fold, "id"]]))
-            test_neg <- rbind(do.call(rbind, encoded_group[fold_list[[3]][fold_list[[3]][, "which"] == fold, "id"]]), 
-                              do.call(rbind, encoded_group[fold_list[[4]][fold_list[[4]][, "which"] == fold, "id"]]))
-            
-            #number of n-grams from protein
-            ngram_prots_pos <- c(sapply(encoded_group[fold_list[[1]][fold_list[[1]][, "which"] == fold, "id"]], nrow),
-                                 sapply(encoded_group[fold_list[[2]][fold_list[[2]][, "which"] == fold, "id"]], nrow))
-            ngram_prots_neg <- c(sapply(encoded_group[fold_list[[3]][fold_list[[3]][, "which"] == fold, "id"]], nrow),
-                                 sapply(encoded_group[fold_list[[4]][fold_list[[4]][, "which"] == fold, "id"]], nrow))
-            
-            preds <- cbind(as.numeric(as.character(predict(model_cv, data.frame(rbind(test_pos, test_neg)[, imp_bigrams]))[["predictions"]])), 
-                           c(rep(1, nrow(test_pos)), rep(0, nrow(test_neg))),
-                           c(unlist(lapply(1L:length(ngram_prots_pos), function(prot_id)
-                             rep(prot_id, ngram_prots_pos[prot_id]))), 
-                             unlist(lapply(1L:length(ngram_prots_neg), function(prot_id)
-                               rep(prot_id, ngram_prots_neg[prot_id])))))
-            
-            # is randomForest consistent with ranger?
-            # model_rf <- randomForest(tar ~ ., train_data)
-            # predict(model_rf, data.frame(rbind(test_pos, test_neg)[, imp_bigrams])) == 
-            #   predict(model_cv, data.frame(rbind(test_pos, test_neg)[, imp_bigrams]))[["predictions"]]
-            
-            list(preds, imp_bigrams)
-          })
-        })
       })
     })
+  }) %>% unlist(recursive = FALSE) %>% 
+    unlist(recursive = FALSE)
+}
+
+# for future reference, positive and negative sequence length in cv
+# lapply(c(6, 10, 15), function(constant_pos) {
+#   lapply(c(6, 10, 15), function(constant_neg) {
+#     lapply(1L:10, function(dummy) {
+#       c(pos = constant_pos, neg = constant_neg)
+#     })
+#   })
+# }) %>% unlist %>% matrix(ncol = 2, byrow = TRUE)
+
+
+#' Perform cross-validation
+#'
+#' Performs cross-validation using the n-grams extracted from amyloids and non-amyloids. 
+#' @param all_folds a list of folds as given by \code{\link{create_all_folds}}.
+#' @param extracted_ngrams a list of n-grams as given by \code{\link{extract_ngrams}}.
+#' 
+#' @note The classifier of choice is the \code{\link{[ranger]ranger}} implementation of 
+#' random forests algorithm.
+#' 
+#' @return List of length equal to the number of repetitions of cross-validation.
+#' Each element of the list is a list with the number of elements equal to the number 
+#' of encodings. Each element of this list contains a list with two elements:
+#' a) data frame with the prediction, real value of target and id of the protein,
+#' b) list of important n-grams as found by \code{\link[biogram]{test_features}}.
+
+do_cv <- function(all_folds, extracted_ngrams)
+  lapply(extracted_ngrams, function(encoded_group)
+    lapply(all_folds, function(fold_list) 
+      do_single_cv(fold_list, encoded_group)
+    )
+  )
+
+
+do_single_cv <- function(fold_list, encoded_group) {
+  lapply(1L:5, function(fold) {
+    
+    #training data
+    train_pos <- do.call(rbind, encoded_group[fold_list[[1]][fold_list[[1]][, "which"] != fold, "id"]])
+    train_neg <- do.call(rbind, encoded_group[fold_list[[3]][fold_list[[3]][, "which"] != fold, "id"]])
+    
+    test_bis <- test_features(c(rep(1, nrow(train_pos)), rep(0, nrow(train_neg))),
+                              rbind(train_pos, train_neg), adjust = NULL)
+    imp_bigrams <- cut(test_bis, breaks = c(0, 0.05, 1))[[1]]
+    
+    train_data <- data.frame(rbind(train_pos, train_neg)[, imp_bigrams],
+                             tar = as.factor(c(rep(1, nrow(train_pos)), rep(0, nrow(train_neg)))))
+    model_cv <- ranger(tar ~ ., train_data, write.forest = TRUE, probability = TRUE)
+    
+    #test data
+    test_pos <- rbind(do.call(rbind, encoded_group[fold_list[[1]][fold_list[[1]][, "which"] == fold, "id"]]),
+                      do.call(rbind, encoded_group[fold_list[[2]][fold_list[[2]][, "which"] == fold, "id"]]))
+    test_neg <- rbind(do.call(rbind, encoded_group[fold_list[[3]][fold_list[[3]][, "which"] == fold, "id"]]), 
+                      do.call(rbind, encoded_group[fold_list[[4]][fold_list[[4]][, "which"] == fold, "id"]]))
+    
+    #number of n-grams from protein
+    ngram_prots_pos <- c(sapply(encoded_group[fold_list[[1]][fold_list[[1]][, "which"] == fold, "id"]], nrow),
+                         sapply(encoded_group[fold_list[[2]][fold_list[[2]][, "which"] == fold, "id"]], nrow))
+    ngram_prots_neg <- c(sapply(encoded_group[fold_list[[3]][fold_list[[3]][, "which"] == fold, "id"]], nrow),
+                         sapply(encoded_group[fold_list[[4]][fold_list[[4]][, "which"] == fold, "id"]], nrow))
+    
+    preds <- cbind(predict(model_cv, data.frame(rbind(test_pos, test_neg)[, imp_bigrams]))[["predictions"]][, 2], 
+                   # probability of being amyloid
+                   c(rep(1, nrow(test_pos)), rep(0, nrow(test_neg))),
+                   c(unlist(lapply(1L:length(ngram_prots_pos), function(prot_id)
+                     rep(prot_id, ngram_prots_pos[prot_id]))), 
+                     unlist(lapply(1L:length(ngram_prots_neg), function(prot_id)
+                       rep(prot_id, ngram_prots_neg[prot_id])))))
+
+    # is randomForest consistent with ranger?
+    # model_rf <- randomForest(tar ~ ., train_data)
+    # predict(model_rf, data.frame(rbind(test_pos, test_neg)[, imp_bigrams])) == 
+    #   predict(model_cv, data.frame(rbind(test_pos, test_neg)[, imp_bigrams]))[["predictions"]]
+    
+    list(preds, imp_bigrams)
   })
 }
