017fc43430dcccc75c965e9427a90c14324339dd
michbur <michalburdukiewicz@gmail.com>
2015-12-28 16:09:26 +0100

    improved report

--- render_report.R
+++ render_report.R
@@ -4,6 +4,9 @@ library(knitr)
 if(Sys.info()["nodename"] == "phobos" )
   pathway <- "/home/michal/Dokumenty/gits/amyloid_ngram2/"
 
+if(Sys.info()["nodename"] == "MICHALKOMP" )
+  pathway <- "D:/GitHub/amyloid_ngram2"
+
 opts_knit$set(root.dir = pathway)
 
 render("./reports/methods.Rmd")
