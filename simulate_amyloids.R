6708e50e8d2551a941f8c5587eb41936252218d5
michbur <michalburdukiewicz@gmail.com>
2015-12-31 22:24:45 +0100

    added simulations of amyloids

--- simulate_amyloids.R
+++ simulate_amyloids.R
@@ -1,4 +1,4 @@
-# Simulate amyloid sequences
+-# Simulate amyloid sequences
 
 # Assumptions:
 # 1. Length of amyloidogenic region: 6 aa
@@ -24,6 +24,41 @@ ftraits <- choose_properties()
 imp_prop <- ftraits[1L:4]
 
 #simulate sequence of length n
-sim_seq <- function(n) {
-  sample(a()[-1], n, replace = TRUE)
+#u - alphabet
+#n - length
+sim_seq <- function(u, n) {
+  sample(u, n, replace = TRUE)
 }
+
+#simulate positive
+#ngram - a VECTOR of ngram
+sim_pos <- function(u, n, ngram) {
+  seq <- sim_seq(u, n)
+  ngram_pos <- sample(1L:(length(seq) - length(ngram) + 1), 1)
+  seq[ngram_pos:(ngram_pos + length(ngram) -1)] <- ngram
+  seq
+}
+
+
+ngrams <- list(c("3", "3", "1"),
+               c("3", "3"),
+               c("3", "1", "3"),
+               c("3", "2", "3"),
+               c("3", "3", "3"))
+
+simulated_seqs <- rbind(do.call(rbind, lapply(1L:200, function(dummy) 
+  sim_pos(as.character(1L:4), 6, sample(ngrams, 1)[[1]]))),
+  do.call(rbind, lapply(1L:200, function(dummy) 
+    sim_seq(as.character(1L:4), 6))))
+  
+
+bitrigrams <- as.matrix(count_multigrams(ns = c(1, rep(2, 4), rep(3, 3)), 
+                                         ds = list(0, 0, 1, 2, 3, c(0, 0), c(0, 1), c(1, 0)),
+                                         seq = simulated_seqs,
+                                         u = 1L:4))
+
+bitrigrams <- bitrigrams > 0
+storage.mode(bitrigrams) <- "integer"
+
+test_bis <- test_features(c(rep(1, 200), rep(0, 200)), bitrigrams, adjust = NULL)
+imp_bigrams <- cut(test_bis, breaks = c(0, 0.05, 1))[[1]]
