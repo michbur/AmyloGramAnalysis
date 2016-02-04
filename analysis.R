da59b722002cc508f399ec0ea04e416581d26510
Piotr Sobczyk <sobbombo@gmail.com>
2016-02-04 08:38:58 +0100

    making code compatible with wcss

--- analysis.R
+++ analysis.R
@@ -6,11 +6,15 @@ source("./functions/cv_analysis.R")
 
 require(seqinr)
 require(dplyr)
-require(pbapply)
-require(biogram)
-require(cvTools)
-require(ranger)
-require(hmeasure)
+require(biogram, lib.loc = "~/R/library")
+require(cvTools, lib.loc = "~/R/library")
+require(ranger, lib.loc = "~/R/library")
+require(hmeasure, lib.loc = "~/R/library")
+
+
+args <- commandArgs(trailingOnly = TRUE)
+idx <- as.numeric(args[1])
+idx2 <- as.numeric(args[2])
 
 ftraits <- choose_properties()
 
@@ -29,8 +33,13 @@ seqs_list <- raw_seqs_list[purified_seqs_id]
 seqs_m <- tolower(t(sapply(seqs_list, function(i)
   c(i, rep(NA, max(lengths(seqs_list)) - length(i))))))
 
+
+ids <- split(1:length(aa_groups), rep(1L:ntiles, each=length(aa_groups)/ntiles+1))
 #for single aa_group around 85 sec
-extracted_ngrams <- extract_ngrams(seqs_m, aa_groups)
+extracted_ngrams <- extract_ngrams(seqs_m, aa_groups[ids[[idx2]]])
+
+#for single aa_group around 85 sec
+#extracted_ngrams <- extract_ngrams(seqs_m, aa_groups)
 #add to extracted_ngrams raw aa n-grams
 
 #create folds for cv
@@ -41,9 +50,15 @@ seq_lengths <- unname(lengths(seqs_list))
 
 all_folds <- create_all_folds(ets, seq_lengths)
 
-cv_results <- do_cv(all_folds, extracted_ngrams, create_hv(seqs_m))
 
-cv_summary <- summarize_cv_results(cv_results, reps_ids = get_reps_ids())
+cv_results <- do_cv(all_folds[idx], extracted_ngrams, create_hv(seqs_m))
+filename <- paste0("./results/cv_results_full_", idx, "_", idx2, ".Rdata")
+print(filename)
+save(cv_results, file = filename)
+print("success")
+
+# cv_results <- do_cv(all_folds, extracted_ngrams, create_hv(seqs_m))
+# cv_summary <- summarize_cv_results(cv_results, reps_ids = get_reps_ids())
 
 # negative control - full amino acid alphabet
 # full_aa <- tolower(a()[-1]) %>% as.list
@@ -52,5 +67,5 @@ cv_summary <- summarize_cv_results(cv_results, reps_ids = get_reps_ids())
 # extracted_ngrams_full <- extract_ngrams(seqs_m, full_aa)
 # cv_results_full <- do_cv(all_folds, extracted_ngrams_full)
 # save(cv_results_full, file = "./results/cv_results_full.RData")
-load("./results/cv_results_full.RData")
+# load("./results/cv_results_full.RData")
 
