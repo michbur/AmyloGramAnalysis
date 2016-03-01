1a844d97d6492d5abe932483864d64d9527c0ff5
michbur <michalburdukiewicz@gmail.com>
2016-03-01 10:23:52 +0100

    added picture

--- functions/cv_analysis2.R
+++ functions/cv_analysis2.R
@@ -61,27 +61,47 @@ enc_part_id_adj <- c(0, cumsum(rep(927, 19)))
 
 all_summaries[["len_range"]] <- factor(all_summaries[["len_range"]], levels(all_summaries[["len_range"]])[c(4, 3, 1, 2)])
 all_summaries <- mutate(all_summaries, 
-       pos = get_reps_ids()[replicate, "pos"],
-       neg = get_reps_ids()[replicate, "neg"],
-       enc_adj = enc + enc_part_id_adj[partition]) %>%
+                        pos = get_reps_ids()[replicate, "pos"],
+                        neg = get_reps_ids()[replicate, "neg"],
+                        enc_adj = enc + enc_part_id_adj[partition]) %>%
   select(len_range, enc_adj, pos, neg, AUC_mean, MCC_mean, Sens_mean, 
-           Spec_mean, AUC_sd, MCC_sd, Sens_sd, Spec_sd, enc, partition)
+         Spec_mean, AUC_sd, MCC_sd, Sens_sd, Spec_sd, enc, partition)
 
 write.csv(all_summaries, file = "./results/all_summaries.csv", row.names = FALSE)
 
 best_sens <- group_by(all_summaries, pos, neg, len_range) %>%
   filter(Sens_mean == max(Sens_mean, na.rm = TRUE)) %>%
   arrange(desc(Sens_mean)) %>%
-  data.frame
-
+  select(pos, neg, len_range, AUC_mean, Sens_mean, Spec_mean, enc_adj) %>%
+  filter(AUC_mean == max(AUC_mean)) %>%
+  mutate(type = "Best sensitivity")
 
 best_spec <- group_by(all_summaries, pos, neg, len_range) %>%
   filter(Spec_mean == max(Spec_mean, na.rm = TRUE)) %>%
   arrange(desc(Spec_mean)) %>%
-  data.frame
+  select(pos, neg, len_range, AUC_mean, Sens_mean, Spec_mean, enc_adj) %>%
+  filter(AUC_mean == max(AUC_mean)) %>%
+  mutate(type = "Best specificity")
 
+bests <- data.frame(rbind(best_sens, best_spec))
 
-best_AUC <- group_by(all_summaries, pos, neg, len_range) %>%
-  filter(AUC_mean == max(AUC_mean, na.rm = TRUE)) %>%
-  arrange(desc(AUC_mean)) %>%
-  data.frame
+group_by(bests, type, enc_adj) %>%
+  summarize(occ = length(enc_adj)) %>%
+  filter(occ > 1) %>%
+  data.frame()
+
+library(ggplot2)
+ggplot(bests, aes(x = factor(pos), y = factor(neg), fill = Sens_mean, label = round(Sens_mean, 2))) +
+  geom_tile() +
+  geom_text(color = "red", size = 5) +
+  facet_grid(type ~ len_range)
+
+png("auc_plot.png", height = 1024, width = 1024)
+ggplot(bests, aes(x = factor(pos), y = factor(neg), fill = AUC_mean, 
+                  label = paste0("Se: ", formatC(Sens_mean, digits = 2, format = "f"), 
+                                 "\n",
+                                 "Sp: ", formatC(Spec_mean, digits = 2, format = "f")))) +
+  geom_tile() +
+  geom_text(color = "red", size = 5) +
+  facet_grid(type ~ len_range)
+dev.off()
