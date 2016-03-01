6009164782871e24f808ffe35dd0125f967950d6
Piotr Sobczyk <sobbombo@gmail.com>
2016-03-01 17:40:20 +0100

    comparing encodings based on their performance for different peptide length

---
+++ analysis_summarized_results.R
@@ -0,0 +1,38 @@
+#amyloidy
+
+library(dplyr)
+
+amyloids <- read.csv("results/all_summaries.csv")
+
+summary(amyloids)
+
+#what kind of AUC can we expect?
+amyloids %>% group_by(len_range) %>% 
+  summarise(max(AUC_mean), mean(AUC_mean), min(AUC_mean)) 
+#there is a huge gain when we take into account that
+#each encoding has different competence regions
+
+#if we would use just one encoding for all then differences in performance are very small
+amyloids %>% group_by(enc) %>% 
+  summarise(AUC_mean=mean(AUC_mean)) %>%
+  summarise(min(AUC_mean), mean(AUC_mean), max(AUC_mean))
+
+#even if we take into account non-homogenity of learning set
+amyloids %>% group_by(enc, pos, neg) %>% 
+  summarise(AUC_mean=mean(AUC_mean)) -> AUC_means
+summary(AUC_means$AUC_mean)
+
+#if we learn on 15-s then we get highest sensitivity. Naturally we pay with specificity
+amyloids %>% group_by(len_range, pos) %>%
+  summarise(max(Sens_mean), mean(Sens_mean), min(Sens_mean),
+            max(Spec_mean), mean(Spec_mean), min(Spec_mean)) 
+
+
+#influence of negative dataset is far less significant
+amyloids %>% group_by(len_range, neg) %>%
+  summarise(max(Sens_mean), mean(Sens_mean), min(Sens_mean),
+            max(Spec_mean), mean(Spec_mean), min(Spec_mean)) 
+
+
+#wychodzi na to, że prawdziwa różnica jest na sensitivity. Różnice są naprawdę duże,
+#co sugeruje, że warto jest robić osobne modele dla każdego typu długości sekwencji
