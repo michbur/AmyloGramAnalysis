731bee25923521bec141c12978a98d374179389a
michbur <michalburdukiewicz@gmail.com>
2016-05-10 16:26:56 +0200

    AmyLoad and reg33 are not in concordance

--- functions/benchmark_functions/reg33_benchmark_other.R
+++ functions/benchmark_functions/reg33_benchmark_other.R
@@ -1,30 +1,30 @@
+# reg33 comes from http://journals.plos.org/plosone/article?id=10.1371/journal.pone.0054175
+# it's named 33, but has 34 proteins
+# add 1 do indices
+# overlapping regions - what to do?
 #read reg33 file
 
 library(dplyr)
 library(seqinr)
 
 
-r33_raw <- readLines("./data/reg33.txt") %>% matrix(ncol = 3, byrow = TRUE) %>% 
-  data.frame(stringsAsFactors = FALSE)
-amyld_ids <- lapply(strsplit(r33_raw[, 3], " "), function(i) matrix(as.numeric(i), nrow = 2))
+r33_raw <- readLines("./benchmark/reg33.txt") %>% matrix(ncol = 3, byrow = TRUE) %>% 
+  data.frame(stringsAsFactors = FALSE) %>% 
+  rename(name = X1, seq = X2, pos = X3)
+hotspot_pos <- lapply(strsplit(r33_raw[, "pos"], " "), function(i) matrix(as.numeric(i), nrow = 2))
 #check the lengths
-whole_seqs <- strsplit(r33_raw[[2]], "")
+r33_seqs <- strsplit(r33_raw[["seq"]], "")
 
-seq_list <- unlist(lapply(1L:length(amyld_ids), function(i) {
-  lapply(1L:ncol(amyld_ids[[i]]), function(j) {
-    id_row <- amyld_ids[[i]][, j]
-    whole_seqs[[i]][id_row[1]:id_row[2] + 1]
-  })
-}), recursive = FALSE)
-
-all_names <- unlist(lapply(1L:length(r33_raw[[1]]), function(i) {
-  names_list <- rep(r33_raw[[1]][i], ncol(amyld_ids[[i]]))
-  if(ncol(amyld_ids[[i]]) > 1) {
-    paste0(names_list, "_", 1L:ncol(amyld_ids[[i]]))
-  } else {
-    names_list
+r33_status <-lapply(1L:length(hotspot_pos), function(i) {
+  # binary peptide (status of amyloidogenicity)
+  bin_peptide <- rep(0, length(r33_seqs[[i]]))
+  for (j in 1L:ncol(hotspot_pos[[i]])) {
+    id_row <- hotspot_pos[[i]][, j]
+    # important +1 to get proper position of 
+    bin_peptide[id_row[1]:id_row[2] + 1] <- 1
   }
-}))
+  bin_peptide
+})
 
 raw_seqs_list <- c(read.fasta("./data/amyloid_pos_full.fasta",seqtype = "AA"),
                    read.fasta("./data/amyloid_neg_full.fasta",seqtype = "AA"))
@@ -32,11 +32,53 @@ raw_seqs_list <- c(read.fasta("./data/amyloid_pos_full.fasta",seqtype = "AA"),
 purified_seqs_id <- lengths(raw_seqs_list) > 5 & lengths(raw_seqs_list) < 26
 seqs_list <- raw_seqs_list[purified_seqs_id]
 
+ets <- c(rep(1, length(read.fasta("./data/amyloid_pos_full.fasta",seqtype = "AA"))),
+         rep(0, length(read.fasta("./data/amyloid_neg_full.fasta",seqtype = "AA"))))
+ets <- ets[purified_seqs_id]
 
-# write.csv2(data.frame(name = all_names, rep33 = sapply(seq_list, paste0, collapse = "")), 
-#            file = "rep33vsAmyLoad.csv",
+# TRUE if the sequence from AmyloGram is also in reg33
+reg33_AmyloGram <- sapply(seqs_list, function(i) {
+  pasted_seq <- paste0(i, collapse = "")
+  sum(grepl(pasted_seq, r33_raw[["seq"]])) > 0
+})
+# table(ets[reg33_AmyloGram])
+# 467 sequences extracted from AmyLoad to train AmyloGram are in reg33 (355 nonamyloids and 112 amyloids)
+
+status_reg33_AmyloGram <- sapply(seqs_list[reg33_AmyloGram], function(single_seq) {
+  pasted_seq <- paste0(single_seq, collapse = "")
+  seq_pos <- gsub(pasted_seq, paste0(rep("_", length(single_seq)), collapse = ""), r33_raw[["seq"]]) %>% 
+    strsplit(split = "") %>% 
+    lapply(function(i) i == "_")
+  
+  search_ids <- which(sapply(seq_pos, sum) > 1)
+  lapply(search_ids, function(single_reg33_seq)
+    r33_status[[single_reg33_seq]][seq_pos[[single_reg33_seq]]]
+  )
+})
+
+decide_reg <- function(x) {
+  ets <- unique(x)
+  if(length(ets) == 1) {
+    ets
+  } else {
+    2
+  }
+}
+
+reg33_al_comp <- data.frame(reg33_seq = sapply(status_reg33_AmyloGram, paste0, collapse = ""),
+           mixed = sapply(status_reg33_AmyloGram, function(single_seq)
+             length(unique(single_seq)) > 1
+           ),
+           AmyLoad_status = ets[reg33_AmyloGram],
+           reg33_status = sapply(status_reg33_AmyloGram, decide_reg)
+)
+
+table(reg33_al_comp[, c("AmyLoad_status", "reg33_status")])
+
+# write.csv2(data.frame(name = all_names, reg33 = sapply(seq_list, paste0, collapse = "")), 
+#            file = "reg33vsAmyLoad.csv",
 #            row.names = FALSE, quote = FALSE)
 # 
 # write.fasta(lapply(seq_list, paste0, collapse = ""), all_names, "reg33.fasta")
-compr <- read.csv2("./data/rep33vsAmyLoad.csv", row.names = NULL, stringsAsFactors = FALSE)
+compr <- read.csv2("./data/reg33vsAmyLoad.csv", row.names = NULL, stringsAsFactors = FALSE)
 
