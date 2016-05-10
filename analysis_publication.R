750f1e915dfbeceb879d57c96b9e6ae681916b2a
michbur <michalburdukiewicz@gmail.com>
2016-05-10 08:12:40 +0200

    reorganized repository

--- analysis_publication.R
+++ analysis_publication.R
@@ -1,8 +1,8 @@
 # load and pre-process necessary data/packages
-source("./functions/pub_functions.R")
+source("./functions/publication_functions/pub_functions.R")
 
 #generate plots
-source("./functions/pub_plots.R")
+source("./functions/publication_functions/pub_plots.R")
 
 #generate tables
-source("./functions/pub_tables.R")
+source("./functions/publication_functions/pub_tables.R")
