f5cca9b8483e28f242be2c782f93f0f8acbb05a7
michbur <michalburdukiewicz@gmail.com>
2016-05-12 13:36:01 +0200

    added status of hexamers

--- functions/benchmark_functions/reg33_benchmark_other.R
+++ functions/benchmark_functions/reg33_benchmark_other.R
@@ -5,7 +5,7 @@
 
 library(dplyr)
 library(seqinr)
-
+library(biogram)
 
 r33_raw <- readLines("./benchmark/reg33.txt") %>% matrix(ncol = 3, byrow = TRUE) %>% 
   data.frame(stringsAsFactors = FALSE) %>% 
@@ -28,7 +28,7 @@ r33_status <-lapply(1L:length(hotspot_pos), function(i) {
   bin_peptide
 })
 
-#lower-upper
+# low- and uppercase sequences to check with the source publication --------------------------------------------
 r33_lu <-lapply(1L:length(hotspot_pos), function(i) {
   new_peptide <- r33_seqs[[i]]
   for (j in 1L:ncol(hotspot_pos[[i]])) {
@@ -38,9 +38,14 @@ r33_lu <-lapply(1L:length(hotspot_pos), function(i) {
   }
   new_peptide
 })
-
 write.fasta(r33_lu, r33_raw[["name"]], "./benchmark/reg33_lu.fasta")
 
+
+# uppercase sequences for benchmark -----------------------------
+write.fasta(r33_seqs, r33_raw[["name"]], "./benchmark/reg33.fasta")
+
+
+# AmyLoad sequences ---------------------------------
 raw_seqs_list <- c(read.fasta("./data/amyloid_pos_full.fasta",seqtype = "AA"),
                    read.fasta("./data/amyloid_neg_full.fasta",seqtype = "AA"))
 #sequences longer than 5 aa and shorter than 26 aa
@@ -50,6 +55,9 @@ seqs_list <- raw_seqs_list[purified_seqs_id]
 ets <- c(rep(1, length(read.fasta("./data/amyloid_pos_full.fasta",seqtype = "AA"))),
          rep(0, length(read.fasta("./data/amyloid_neg_full.fasta",seqtype = "AA"))))
 ets <- ets[purified_seqs_id]
+
+
+# comparison of AmyLoad and reg33 ----------------------------------
 
 # TRUE if the sequence from AmyloGram is also in reg33
 reg33_AmyloGram <- sapply(seqs_list, function(i) {
@@ -83,21 +91,34 @@ decide_reg <- function(x) {
 reg33_al_comp <- data.frame(seq_name = names(seqs_list[reg33_AmyloGram]),
                             seq = sapply(seqs_list[reg33_AmyloGram], paste0, collapse = ""),
                             reg33_status = sapply(status_reg33_AmyloGram, paste0, collapse = ""),
-                            mixed = sapply(status_reg33_AmyloGram, function(single_seq)
-                              length(unique(single_seq)) > 1
-                            ),
                             AmyLoad_et = ets[reg33_AmyloGram],
                             reg33_et = sapply(status_reg33_AmyloGram, decide_reg),
                             stringsAsFactors = FALSE
 ) %>% mutate(AmyLoad_et = factor(AmyLoad_et, labels = c("Non-amyloid", "Amyloid")),
-             AmyLoad_et = factor(AmyLoad_et, labels = c("Non-amyloid", "Amyloid")))
+             reg33_et = factor(reg33_et, labels = c("Non-amyloid", "Amyloid", "Mixed")))
 
-table(reg33_al_comp[, c("AmyLoad_et", "reg33_et")])
+as.data.frame(table(reg33_al_comp[, c("AmyLoad_et", "reg33_et")]), responseName = "Count")
 
-# write.csv2(data.frame(name = all_names, reg33 = sapply(seq_list, paste0, collapse = "")), 
-#            file = "reg33vsAmyLoad.csv",
-#            row.names = FALSE, quote = FALSE)
-# 
-# write.fasta(lapply(seq_list, paste0, collapse = ""), all_names, "reg33.fasta")
-compr <- read.csv2("./data/reg33vsAmyLoad.csv", row.names = NULL, stringsAsFactors = FALSE)
+# training of AmyloGram ---------------------------
+
+seqs_list[!reg33_AmyloGram]
+ets[!reg33_AmyloGram]
+
+seq_hex <- seq2ngrams(r33_seqs[[1]], 6, a()[-1]) %>% 
+  decode_ngrams() %>% 
+  strsplit("") %>% 
+  do.call(rbind, .)
+
+seq_hex_status <- seq2ngrams(r33_status[[1]], 6, C(0, 1)) %>% 
+  decode_ngrams() %>% 
+  strsplit("") %>% 
+  do.call(rbind, .) %>% 
+  as.numeric %>% 
+  matrix(ncol = 6) 
+
+lapply(1L:nrow(seq_hex_status), function(single_hex_id)
+  c(rep(NA, single_hex_id - 1), seq_hex_status[single_hex_id, ], rep(NA, nrow(seq_hex_status) - single_hex_id))
+) %>% 
+  do.call(rbind, .) %>% 
+  colMeans(na.rm = TRUE)
 
