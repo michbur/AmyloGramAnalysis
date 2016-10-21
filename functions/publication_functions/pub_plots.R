e77a66aea85029fc0a3a3400b0b9c515e4a4fb1b
michbur <michalburdukiewicz@gmail.com>
2016-10-21 16:17:30 +0200

    converted figures to black and white

--- functions/publication_functions/pub_plots.R
+++ functions/publication_functions/pub_plots.R
@@ -133,14 +133,15 @@ for(i in 1L:length(sesp_plots)) {
 AUC_boxplot <- ggplot(amyloids_plot, aes(x = len_range, y = AUC_mean)) +
   geom_boxplot(outlier.color = "grey", outlier.shape = 1, outlier.size = 1) +
   geom_point(data = filter(amyloids_plot, et2 != "Encoding"), 
-             aes(x = len_range, y = AUC_mean, color = et2, shape = et2, size = et2),
+             aes(x = len_range, y = AUC_mean, shape = et2, size = et2),
              fill = NA) +
   scale_x_discrete("") +
   scale_y_continuous("Mean AUC") +
-  guides(color = guide_legend(nrow = 2), shape = guide_legend(nrow = 2)) +
+  guides(color = guide_legend(nrow = 3), shape = guide_legend(nrow = 3)) +
   scale_shape_manual("", values = c(1, 21, 23, 24, 25), drop = FALSE) +
-  scale_color_manual("", values = c("grey", "firebrick1", "green3", "dodgerblue", "dodgerblue"), drop = FALSE) +
-  scale_size_manual("", values = c(0.5, 0.5, 0.5, 0.5, 0.5) + 0.5, drop = FALSE) +
+  #scale_color_manual("", values = c("grey", "firebrick1", "green3", "dodgerblue", "dodgerblue"), drop = FALSE) +
+  scale_color_manual("", values = c("grey", rep("black", 4)), drop = FALSE) +
+  scale_size_manual("", values = c(1, 1.5, 1.5, 1.5, 1.5), drop = FALSE) +
   facet_wrap(~ pos, nrow = 3) +
   my_theme + 
   coord_flip() 
@@ -361,7 +362,7 @@ write.csv2(si_dat, row.names = FALSE, file = "./results/si_dat.csv")
 
 si_AUC_plot <- ggplot(si_dat, aes(x=si, y=AUC_mean)) + 
   geom_bin2d(bins = 25, color = "black") + 
-  scale_fill_gradient2("Number of encodings", low = "beige", mid = "orange3", high = "deeppink1",
+  scale_fill_gradient2("Number of encodings", low = "white", high = "grey",
                        midpoint = 500) +
   xlab("Similarity to the best-performing encoding\n") +
   ylab("AUC") +
@@ -372,8 +373,9 @@ si_AUC_plot <- ggplot(si_dat, aes(x=si, y=AUC_mean)) +
   guides(color = guide_legend(nrow = 4), shape = guide_legend(nrow = 4), 
          fill = guide_colorbar(barwidth = unit(6, "line"))) +
   scale_shape_manual("", values = c(21, 23, 24, 25), drop = FALSE) +
-  scale_color_manual("", values = c("firebrick1", "green3", "dodgerblue", "dodgerblue"), drop = FALSE) +
-  scale_size_manual("", values = c(0.5, 0.5, 0.5, 0.5) + 0.5, drop = FALSE)
+  #scale_color_manual("", values = c("firebrick1", "green3", "dodgerblue", "dodgerblue"), drop = FALSE) +
+  scale_color_manual("", values = rep("black", 4), drop = FALSE) +
+  scale_size_manual("", values = c(0.5, 0.5, 0.5, 0.5) + 1.5, drop = FALSE) 
 
 cairo_ps("./publication/figures/ed_AUC.eps", height = 3.5, width = 3)
 print(si_AUC_plot)
